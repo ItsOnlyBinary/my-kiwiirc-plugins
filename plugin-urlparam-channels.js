6e87b98c64006fde7913efa647132438abc9e27f
ItsOnlyBinary <git@itsonlybinary.com>
2021-12-16 14:28:25 +0000

    Add plugin-urlparam-channels.js

---
+++ plugin-urlparam-channels.js
@@ -0,0 +1,22 @@
+kiwi.plugin('url-channels', function(kiwi, log) {
+    const Misc = kiwi.require('helpers/Misc');
+
+    kiwi.on('network.connecting', function (event) {
+        const network = event.network;
+        const channels = (Misc.queryStringVal('channels') || '')
+            .trim()
+            .split(',')
+            .filter(function (channel) { return !!channel });
+
+        channels.forEach(function (channel) {
+            const activeBuffer = kiwi.state.getActiveBuffer(network.id);
+            const bufferName = (channel[0] !== '#') ? '#' + channel : channel;
+            const newBuffer = kiwi.state.addBuffer(network.id, bufferName);
+            newBuffer.enabled = true;
+
+            if (activeBuffer && !activeBuffer.isChannel()) {
+                kiwi.state.setActiveBuffer(network.id, newBuffer.name);
+            }
+        });
+    });
+});
